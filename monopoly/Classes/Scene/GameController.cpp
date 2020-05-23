01724e4e0fc97fdd75d0877925fa5f639cf18ab0
xmx <1426356297@qq.com>
2020-05-23 17:21:24 +0800

    feat(Scene):设计GameController类最基础的接口

--- monopoly/Classes/Scene/GameController.cpp
+++ monopoly/Classes/Scene/GameController.cpp
@@ -1,1 +1,23 @@
+#include "GameController.h"
 
+GameController* GameController::game_controller_instance_ = NULL;
+
+bool GameController::init()
+{
+	if (!Layer::init())
+	{
+		return false;
+	}
+	//Î´Íê
+	return true;
+}
+
+GameController* GameController::getInstance()
+{
+	if (!game_controller_instance_)
+	{
+		game_controller_instance_ = GameController::create();
+	}
+	game_controller_instance_->retain();
+	return game_controller_instance_;
+}
