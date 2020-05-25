2d61db52896435873d8387b5a86e69e28103e8d6
Mizu <2579587457@qq.com>
2020-05-25 22:42:52 +0800

    feat(Finance)增加金钱增减事件

---
+++ monopoly/Classes/Incident/Finance.cpp
@@ -0,0 +1,19 @@
+#include "Incident.h"
+
+void Incident::FinanceGain(Character* player) {
+	auto money = (*player).getMoney();
+	money += 5000;
+	(*player).setMoney(money);
+	                                             //»ñµÃ5000
+}
+void Incident::FinanceLost(Character* player) {
+	auto money = (*player).getMoney();
+	if (money >= 5000) {
+		money -= 5000;
+	}
+	else {
+		money = 0;
+	}
+	(*player).setMoney(money);
+	                                             //¶ªÊ§5000
+}
