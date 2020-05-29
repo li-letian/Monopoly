011558797c5dfc5446c78db446ad1adb5850154d
Mizu <2579587457@qq.com>
2020-05-29 16:39:47 +0800

    feat(StorkScene)增加股票内容

--- monopoly/Classes/Scene/StorkScene.cpp
+++ monopoly/Classes/Scene/StorkScene.cpp
@@ -1,12 +1,12 @@
 #include "StorkScene.h"
+#include "ZH.h"
+#include "Character/Character.h"
 
-
-
-Stock* Stock::creat(int stockCode_, std::string stockName, int nowPrice, int makeDealPrice, float percent, int storeNumber) {
+Stock* Stock::create(int stockCode_, std::string stockName, int nowPrice, int makeDealPrice, float percent, int storeNumber) {
 	Stock* stock = new Stock(stockCode_,stockName,nowPrice,makeDealPrice,percent,storeNumber);
-	stock->init();
-	stock->autorelease();
-	return stock;
+	//stock->init();
+	//stock->autorelease();
+	//return stock;
 }
 Stock::Stock(int stockCode_, std::string stockName_, int nowPrice_, int makeDealPrice_, float percent_, int storeNumber_) {
 	this->stockCode_ = stockCode_;
@@ -16,5 +16,50 @@ Stock::Stock(int stockCode_, std::string stockName_, int nowPrice_, int makeDeal
 	this->storeNumber_ = storeNumber_;
 	this->makeDealPrice_ = makeDealPrice_;
 	
+
+}
+Scene* StockScene::createScene() {
+	auto scene = Scene::create();
+	auto layer = StockScene::create();
+	scene->addChild(layer);
+	return scene;
+
+}
+bool StockScene::init() {
+	if (!Layer::init()) {
+		return false;
+	}
+	initLabel(Character::getStockVec());                       //考虑传进来vec
+	return true;
+
+
+}
+void StockScene::initLabel(Vector<Stock*> vec) {
+	Size VisibleSize = Director::getInstance()->getVisibleSize();	//获得屏幕大小
+	for (int i = 0; i < 4; i++) {
+		Value valCode, valName, valPrice, valDeal,valPer,valLabel;
+		valCode = Value(vec.at(i)->stockCode_);
+		valName = Value(vec.at(i)->stockName_);
+		valPrice = Value(vec.at(i)->nowPrice_);
+		valDeal = Value(vec.at(i)->makeDealPrice_);
+		valPer = Value(vec.at(i)->percent_);
+		Value valBlank(" ");
+		
+		valLabel = valCode.asString().c_str + valBlank.asString()+valName.asString() + valBlank.asString() +
+			valPrice.asString().c_str() + valBlank.asString() + valDeal.asString().c_str() + valBlank.asString() +
+			valPer.asString().c_str();
+
+		auto label = Label::createWithSystemFont(valLabel.asString().c_str(), "fonts/arial.ttf", 30);//创建一个标签
+		label->setTextColor(Color4B::GREEN);
+		label->setAnchorPoint(Vec2(0, 0));
+		label->setPosition(Vec2(0, VisibleSize.height - 36*(1+i)));//设置位置
+		this->addChild(label);
+
+
+	}
+
+
+
+
 
 }
--- monopoly/Classes/Scene/StorkScene.h
+++ monopoly/Classes/Scene/StorkScene.h
@@ -1,9 +1,11 @@
-#ifndef __STORK_SCENE_H__
-#define __STORK_SCENE_H__
-#endif
+#ifndef _STORK_SCENE_H_
+#define _STORK_SCENE_H_
+#include "ZH.h"
 #include "cocos2d.h"
+using namespace cocos2d;
+
 class Stock :public cocos2d::Sprite {
-private:
+public:
 	int stockCode_;            //股票代码
 	std::string stockName_;    //股票名称
 	int nowPrice_;             //当前价格
@@ -13,34 +15,17 @@ private:
 
 public:
 	Stock(int stockCode_, std::string stockName_, int nowPrice_, int makeDealPrice_, float percent_, int storeNumber_);
-	Stock* creat(int stockCode_, std::string stockName_, int nowPrice_, int makeDealPrice_, float percent_, int storeNumber_);
+	static Stock* create(int stockCode_, std::string stockName_, int nowPrice_, int makeDealPrice_, float percent_, int storeNumber_);
 	
 	 
 };
-class StockCellCard :public cocos2d::Sprite {
+
+
+class StockScene :public Layer {
 public:
-	static StockCellCard* createCardSprite(int numbers, int width, int height, float spriteX, float spriteY);
+	static Scene* createScene();
 	virtual bool init();
-	CREATE_FUNC(StockCellCard);
-	int getStockNumber();
-	void setStockNumber(int num);
-private:
-	Label* stockLabel;
-	LayerColor* layerColor;
+	CREATE_FUNC(StockScene);
+	void initLabel(Vector<Stock*> vec);
 };
-
-
-
-
-//TableView:
-const int stockCellWidth = 100;
-const int stockCellHeight = 30;
-const int buy_button = 801;
-const int sell_button = 802;
-const int back_button = 803;
-const float spaceRatio = 0.08;
-const float leftRatioX = 0.1;
-
-class LineChart :public LayerColor, TableViewDataSource, TableViewDelegate {
-
-};
+#endif
