60412e43995b568390cdc74e1cb71ce2a9b7a663
Mizu <2579587457@qq.com>
2020-06-09 16:52:17 +0800

    fix(Item)乱码更正

--- monopoly/Classes/Item/Item.h
+++ monopoly/Classes/Item/Item.h
@@ -7,7 +7,7 @@ USING_NS_CC;
 class Character;
 class Item : public Sprite {
 private:
-	CC_SYNTHESIZE(std::string, name_, ItemName);	//����
+	CC_SYNTHESIZE(std::string, name_, ItemName);	//道具名称
 public:
 	virtual void work(Character* player) = 0;
 
